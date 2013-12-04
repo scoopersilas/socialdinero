408d7be8252fe4f4dfa268a4efa5a12e4b4ea00c
Shana Silas <chefshana@me.com>
2013-12-03 23:47:19 -0500

    Inital Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Socialdinero::Application.config.secret_key_base = '9262328b373c958a0cbf5e8c6d635cde469628ba29993c245655f78c8141ff13973a04ebeec5bfd901f9a3cf79343c151bbffa050f063c94157f1c016277b7e2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Socialdinero::Application.config.session_store :cookie_store, key: '_socialdinero_session'
